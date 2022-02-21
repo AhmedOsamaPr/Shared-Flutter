cf6ab214d159c5007a338f9d7e932fb5eadfd295
Osama <a.osama@waslabrowser.com>
2022-02-21 16:50:09 +0200

    Handle new appbar

--- lib/core/ui_kit/toolbar_kit.dart
+++ lib/core/ui_kit/toolbar_kit.dart
@@ -1,54 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shared_flutter/core/styles/text_style_weight_factory.dart';
+import 'package:shared_flutter/core/ui_kit/wasla_colors.dart';
 
-import '../ui_kit/wasla_colors.dart';
+class PrimaryToolBar extends AppBar {
+  final String text;
 
-
-
-class PrimaryToolBar extends StatelessWidget {
-  final String title;
-
-  final List<Widget>? actions;
-
-  const PrimaryToolBar({Key? key, required this.title, this.actions})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(
-        title,
-        style: WaslaTextStyle.medium(Size.TEXT_XL),
-      ),
-      actions: actions ?? const [],
-    );
-  }
+  PrimaryToolBar({Key? key, required this.text})
+      : super(key: key, title: Text(text, style: WaslaTextStyle.medium(Size.TEXT_XL)));
 }
 
-class SecondaryToolBar extends StatelessWidget {
-  final String title;
-  final List<Widget>? actions;
+class SecondaryToolBar extends AppBar {
+  final String text;
   final VoidCallback onPressed;
 
-  const SecondaryToolBar(
-      {Key? key, required this.onPressed, required this.title, this.actions})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-        icon: const Icon(
-          Icons.arrow_back,
-          color: WaslaColors.gray_1,
-        ),
-        onPressed: onPressed,
-      ),
-      title: Text(
-        title,
-        style: WaslaTextStyle.medium(Size.TEXT_XL),
-      ),
-      actions: actions,
-    );
-  }
+  SecondaryToolBar(
+      {Key? key, required this.onPressed, required this.text})
+      : super(key: key, title: Text(text, style: WaslaTextStyle.medium(Size.TEXT_XL))
+      , leading: IconButton(onPressed: onPressed, icon: const Icon(
+        Icons.arrow_back, color: WaslaColors.gray_1,
+      ))
+  );
 }
